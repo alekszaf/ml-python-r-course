e7e381797deaa49b725f2bebefb2ea896ae1402d
alekszaf <a.zaforemska2@newcastle.as.uk>
2021-04-13 21:56:51 +0200

    Add R introductory tutorial

---
+++ Section 3 - Setting up R.R
@@ -0,0 +1,13 @@
+#Demo content
+
+print('Hello world')
+
+x <- 2
+x <- 3 # You can also use '=' to assign a variable, but it is 'frowned upon'
+
+y <- c(1,2,3,4,5) # Creates an array of numbers with concatination oprator 'c'
+y <- 1:10 # Same result as above
+
+x <- y <- 1:10 # Create two arrays of the same length
+
+x + y # Add two arrays: each element from one array will be added to the corresponding element of the second array
