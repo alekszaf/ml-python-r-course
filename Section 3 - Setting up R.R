b109abb8708c8eabd2965a42c890c24badc9f31a
alekszaf <a.zaforemska2@newcastle.as.uk>
2021-04-13 23:23:17 +0200

    Loading data in R

--- Section 3 - Setting up R.R
+++ Section 3 - Setting up R.R
@@ -15,10 +15,52 @@ z <- x + y # Add two arrays: each element from one array will be added to the co
 
 # Packages
 
-library(ggplot2) # Load ggplot2
-
 browseURL("https://cran.r-project.org/web/views/") # Check the list of available packages on CRAN
 
 install.packages("LiblineaR") # Install library for linear predictive models
 
 library() # Gives list of all the packages installed
+search() # Show currently loaded packages
+
+library(ggplot2) # Load packages (returns error if the package doesn't exist)
+require("LiblineaR") # Load the package (usually used inside functions, returns logical value 'FALSE' if the package doesn't exist)
+
+detach("package:LiblineaR", unload=TRUE) # Unload the package
+
+
+# Inputting data from inbuilt dataset
+
+data() # show list of inbuilt datasets
+
+? iris # show description of the dataset
+str(iris) # show structure of iris dataset
+
+data("iris") # load iris dataset into the workspace
+
+
+# Manual entry of data
+
+x1 = 1:10
+x2 <- c(2, 5, 7, 4)
+x3 <- seq(5, 50, 5) # sequence from 5 to 50 with increment of 5
+x4 <- scan() # manually enter values one by one
+
+
+# Import data from file
+
+Product <- read.table(
+  "C:/Users/b7079552/OneDrive - Newcastle University/Maths and coding/Machine learning/Machine learning in Python and R/Data Files/1. ST Academy - Crash course and Regression files/Product.txt", header = TRUE, sep = "\t")
+
+str(Product)
+
+Customer <-read.csv(
+  "C:/Users/b7079552/OneDrive - Newcastle University/Maths and coding/Machine learning/Machine learning in Python and R/Data Files/1. ST Academy - Crash course and Regression files/Customer.csv", header = TRUE)
+
+str(Customer)
+View(Customer) # Show the full table of the dataset
+
+
+# Plotting
+
+y <- table(Customer$Region) # Show aggregate of values in Region column
+
